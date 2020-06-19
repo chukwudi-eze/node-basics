5362976e2008ea210b2271d63c456c88f16cc2ca
Chukwudi Eze <chukwudieze97@gmail.com>
2020-06-19 14:51:40 +0100

    basic node tutorials

---
+++ fs-demo.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+
+//Create folder
+//fs.mkdir(path.join(__dirname, '/test'), 
+//{}, 
+//err => {
+ //   if (err) throw err; 
+  //  console.log("File created....")
+// });
+
+//Create and write to file
+//fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 
+//'Hello World!', 
+//err => {
+  //  if (err) throw err; 
+    //console.log("File written to....")
+//});
+
+//Read file
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 
+"utf8", 
+(err, data) => {
+    if (err) throw err; 
+    console.log(data)
+});
